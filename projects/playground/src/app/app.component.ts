953596a136ba231bb020afb21d24e2f4853e0596
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-04-04 16:24:43 +0200

    Les limites de l'exploitation de formulaires

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'playground';
+  @ViewChild('email') emailInput?: ElementRef<HTMLInputElement>
+
+  onSubmit(event: SubmitEvent): void {
+    event.preventDefault();
+    console.log(this.emailInput?.nativeElement.value);
+  }
 }
