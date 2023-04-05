8c457ea1d440a28ae509961284db8638bcc8c8ae
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-04-05 13:00:38 +0200

    Implémenter l'interface ControlValueAcessor

--- projects/playground/src/app/components/color-picker/color-picker.component.ts
+++ projects/playground/src/app/components/color-picker/color-picker.component.ts
@@ -1,16 +1,46 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.css']
+  styleUrls: ['./color-picker.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ColorPickerComponent,
+      multi: true
+    }
+  ]
 })
-export class ColorPickerComponent implements OnInit {
-  @Input()
-  color ='blue';
-  constructor() { }
+export class ColorPickerComponent implements ControlValueAccessor {
+  @Input() color ='blue';
+  @Input() disabled = false;
 
-  ngOnInit(): void {
+  onChange: (_: any) => void = (_: any) => {}
+  onTouched: () => void = () => {}
+
+
+  setColor(color: string): void {
+    this.color = color;
+    this.onChange(this.color);
+    this.onTouched();
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  writeValue(color: string): void {
+    this.color = color;
   }
 
 }
