5bd403029112b3d9dc98d2ac8313e3be22f8949f
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-04-06 11:28:26 +0200

    RF : Validation asynchrone en approche Réactive

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {AbstractControl, FormControl, ValidatorFn, Validators} from "@angular/forms";
+import {AbstractControl, AsyncValidatorFn, FormControl, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -10,9 +10,12 @@ export class AppComponent {
   email = new FormControl('', [
     Validators.required,
     Validators.email,
-    createBannedEmailValidator('test@test.com')
+    createBannedEmailValidator('test@test.com'),
+  ], [
+    uniqueEmailValidator
   ]);
 }
+
 const createBannedEmailValidator= (bannedEmail: string): ValidatorFn => {
   return (control: AbstractControl<string>) => {
     if (control.value === bannedEmail) {
@@ -22,4 +25,14 @@ const createBannedEmailValidator= (bannedEmail: string): ValidatorFn => {
   };
 }
 
+const uniqueEmailValidator: AsyncValidatorFn = (control:AbstractControl<string>) => {
+  return fetch(`https://jsonplaceholder.typicode.com/users?email=${control.value}`)
+    .then(resp => resp.json())
+    .then((users: any[]) =>{
+      if (users.length > 0) {
+        return { uniqueEmail: true };
+      }
+      return null;
+  })
+}
 
