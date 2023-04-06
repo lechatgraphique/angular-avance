5c4c61241b5db7863c7f2316ffd99332f1e9a50a
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-04-06 08:55:37 +0200

    NgModel et DataBinding

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -8,7 +8,12 @@ import {NgForm} from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild('email') emailInput?: ElementRef<HTMLInputElement>
-
+  data = {
+    email: 'hello@lechatgraphique.fr',
+    color: 'blue',
+    password: 'azerty',
+    confirm: 'azerty'
+  }
   onSubmit(form: NgForm): void {
     console.log(form.value);
   }
