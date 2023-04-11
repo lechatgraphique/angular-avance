25980df058225bb02bea887c67b99bd02fe8bdaf
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-04-11 10:07:56 +0200

    Typages forts et transformations

--- projects/playground/src/app/components/movies/movies.component.ts
+++ projects/playground/src/app/components/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {ApiMovie, ApiPopularResponce, Movies} from "../../types/types";
 
 @Component({
   selector: 'app-movies',
@@ -7,14 +8,22 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-  movies: any[] = [];
+  movies: Movies = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    const responce = this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=9fcfd84b49752709e1ee2080f0448675&language=fr-FR&page=1');
+    const request = this.http.get<ApiPopularResponce>('https://api.themoviedb.org/3/movie/popular?api_key=9fcfd84b49752709e1ee2080f0448675&language=fr-FR&page=1');
 
-    responce.subscribe((res: any) => {
-      this.movies = res.results;
+    request.subscribe((response) => {
+      this.movies = response.results.map(item => {
+        return {
+          id: item.id,
+          title: item.title,
+          description: item.overview,
+          rating: item.vote_average,
+          image: `https://image.tmdb.org/t/p/w500/${item.poster_path}`
+        }
+      })
     })
   }
 }
